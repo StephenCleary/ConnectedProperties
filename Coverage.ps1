bb4d3c0d49d05ad480d6c6a394f876151d1ba2e3
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-04-09 21:48:14 -0400

    Change to release build for unit tests.

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,4 +1,4 @@
 $testProjectLocations = @('test/UnitTests')
 $outputLocation = 'testResults'
-$dotnetTestArgs = '-c DebugCI'
+$dotnetTestArgs = '-c Release'
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/StephenCleary/BuildTools/599beba35b53f495d4df6e5c323573aa839137a3/Coverage.ps1'))
