ece80bdce924ae9dfbc8b3e17d1733c41350b62d
Stephen Cleary <StephenCleary@users.noreply.github.com>
2017-04-09 20:15:27 -0400

    Add usage examples for double-checking doc examples.

---
+++ test/UnitTests/UsageExamples.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static Nito.ConnectedProperties.ConnectedProperty;
+
+namespace UnitTests
+{
+    class UsageExamples
+    {
+        [Fact]
+        public void SimpleUsage()
+        {
+            var obj = new object();
+
+            var nameProperty = GetConnectedProperty(obj, "Name");
+            nameProperty.Set("Bob");
+
+            Assert.Equal("Bob", ReadName(obj));
+        }
+
+        private static string ReadName(object obj)
+        {
+            var nameProperty = GetConnectedProperty(obj, "Name");
+            return nameProperty.Get();
+        }
+    }
+}
